599ff2ccfbd29a2d49ab1cfd94a052a7a660e188
Ilia Cimpoes <Ilia.Cimpoes@endava.com>
2016-01-13 10:00:15 +0200

    sorting

---
+++ src/main/scala/training/scalaCore/sort/package.scala
@@ -0,0 +1,38 @@
+//package training.scalaCore
+//
+//import scala.annotation.tailrec
+//
+//package object sort {
+//
+//  /**
+//   * quick sort implementation
+//   */
+//  def quickSort(list: List[Int]): List[Int] = {
+//    if (list.length <= 1) {
+//      list
+//    } else {
+//      val (head :: tail) = list
+//      quickSort(tail.filter(_ <= head)) ::: head :: quickSort(tail.filter(_ > head))
+//    }
+//  }
+//
+//  /**
+//   * quick sort implementation
+//   */
+//  @tailrec
+//  def quickSortTailRec(list: List[Int]): List[Int] = {
+//
+//    def temp(l1: List[Int], acc: List[Int]): List[Int] = {
+//      if (list.length <= 1) {
+//        acc
+//      } else {
+//        val (head :: tail) = list
+//
+////        quickSort(tail.filter(_ <= head)) ::: head :: quickSort(tail.filter(_ > head))
+//      }
+//    }
+//
+//    temp(list, Nil)
+//  }
+//
+//}
