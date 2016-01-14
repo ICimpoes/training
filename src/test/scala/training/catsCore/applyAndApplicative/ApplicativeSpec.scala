4e7bdca762780ce921e09c3dfba66694902717c0
Ilia Cimpoes <Ilia.Cimpoes@endava.com>
2016-01-14 14:28:24 +0200

    Small adjustments in Apply and Applicative

--- src/test/scala/training/catsCore/applyAndApplicative/ApplicativeSpec.scala
+++ src/test/scala/training/catsCore/applyAndApplicative/ApplicativeSpec.scala
@@ -7,30 +7,31 @@ import training.catsCore.functor.{NoValue, Value, Wrapper}
 class ApplicativeSpec extends WordSpec with Matchers {
 
   "An Applicative" when {
+    val F = Applicative[Wrapper]
     "calling pure" should {
       "return a Value[A]" in {
-        Applicative[Wrapper].pure(10) shouldBe Value(10)
-        Applicative[Wrapper].pure("") shouldBe Value("")
+        F.pure(10) shouldBe Value(10)
+        F.pure("") shouldBe Value("")
       }
     }
     "calling map on a Value" should {
       "return mapped Value" in {
         val v = Value(56)
-        Applicative[Wrapper].map(v)(_ + 95 / 5) shouldBe Value(56 + 95 / 5)
+        F.map(v)(_ + 95 / 5) shouldBe Value(56 + 95 / 5)
+        F.map(v)(_ + 95 / 5) shouldBe Value(56 + 95 / 5)
       }
     }
     "calling map on the NoValue" should {
       "return NoValue" in {
         val n = NoValue
-        Applicative[Wrapper].map(n)(_.toString) shouldBe NoValue
+        F.map(n)(_.toString) shouldBe NoValue
       }
     }
     "calling sequenceA on List[Int => Int]" should {
       "return Int => List[Int]" in {
-        import cats.std.list._
         import cats.std.function._
         type F[A] = (Int) => A
-        val f: F[List[Int]] = sequenceA[F, Int](List((_: Int) + 3, (_: Int) + 2, (_: Int) + 1))
+        val f: F[List[Int]] = sequenceA[F, Int](List(_ + 3, _ + 2, _ + 1))
         f(3) shouldBe List(3 + 3, 3 + 2, 3 + 1)
       }
     }
--- src/test/scala/training/catsCore/applyAndApplicative/ApplySpec.scala
+++ src/test/scala/training/catsCore/applyAndApplicative/ApplySpec.scala
@@ -1,42 +1,44 @@
 package training.catsCore.applyAndApplicative
 
-import cats.{Functor, Apply}
+import cats._
+import cats.syntax.apply._
 import org.scalatest.{Matchers, WordSpec}
 import training.catsCore.functor.{wrapperFunctor => _, _}
 
 class ApplySpec extends WordSpec with Matchers {
 
   "Apply[?]" should {
+    val F = Apply[Wrapper]
     "correctly apply List" in {
       import cats.std.list.listInstance
       val fs = List(1, 2, 3) map { x => x * 3 + (_: Int) } // == {(_: Int) * 3 + (_: Int)}.curried
       val vs = List(5, 10)
-      Apply[List].ap(vs)(fs) shouldBe List(8, 13, 11, 16, 14, 19)
+      vs.ap(fs) shouldBe List(8, 13, 11, 16, 14, 19)
     }
     "correctly apply Value" in {
-      val f: Wrapper[Int => String] = Value { x: Int => (x * 2 + 2).toString }
+      val f = Value { x: Int => (x * 2 + 2).toString }
       val v = Value(6)
-      Apply[Wrapper].ap(v)(f) shouldBe Value((6 * 2 + 2).toString)
+      F.ap(v)(f) shouldBe Value((6 * 2 + 2).toString)
     }
     "correctly apply Value" when {
       "using Functor to construct Wrapper[A => B]" in {
         //f(x) = (x + 3) * 5
-        val f: Wrapper[Int => Int] = Functor[Wrapper].map(Value[Int](5)) { x => ((_: Int) + 3) andThen (_ * x) }
+        val f = Functor[Wrapper].map(Value[Int](5)) { x => ((_: Int) + 3) andThen (_ * x) }
         val v = Value(6)
-        Apply[Wrapper].ap(v)(f) shouldBe Value((6 + 3) * 5)
-        Apply[Wrapper].ap(NoValue)(f) shouldBe NoValue
-        Apply[Wrapper].ap(Value(0))(f) shouldBe Value(3 * 5)
+        F.ap(v)(f) shouldBe Value((6 + 3) * 5)
+        F.ap(NoValue)(f) shouldBe NoValue
+        F.ap(Value(0))(f) shouldBe Value(3 * 5)
       }
     }
     "correctly apply NoValue as a F[A => B]" in {
-      val f = NoValue
+      val f: Wrapper[Int => Nothing] = NoValue
       val v = Value(6)
-      Apply[Wrapper].ap(v)(f) shouldBe NoValue
+      F.ap(v)(f) shouldBe NoValue
     }
     "correctly apply NoValue as a F[A]" in {
       val f = Value((_: Int) * 2 + 2)
       val v = NoValue
-      Apply[Wrapper].ap(v)(f) shouldBe NoValue
+      F.ap(v)(f) shouldBe NoValue
     }
   }
 }
